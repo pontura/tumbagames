9a3ec3a74c2183d4919bba3631dca7295873ec4d
pontura <pontura@gmail.com>
2016-05-13 12:12:28 -0300

    nuevas colisiones

---
+++ pungaRaid/src/game/Assets/CollisionWithBorders.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionWithBorders : MonoBehaviour {
+
+    void Update()
+    {
+        RaycastHit[] hits;
+        Vector3 pos = transform.localPosition;
+        pos.z = -10;
+        hits = Physics.RaycastAll(pos, transform.forward, 100.0F);
+
+        print(pos +  " transform.forward: " + transform.forward + " hits: " + hits.Length);
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+
+            print(hit.transform.gameObject.name);
+
+            //Renderer rend = hit.transform.GetComponent<Renderer>();
+
+            //if (rend)
+            //{
+            //    // Change the material of all hit colliders
+            //    // to use a transparent shader.
+            //    rend.material.shader = Shader.Find("Transparent/Diffuse");
+            //    Color tempColor = rend.material.color;
+            //    tempColor.a = 0.3F;
+            //    rend.material.color = tempColor;
+            //}
+        }
+    }
+}
---
+++ pungaRaid/src/game/Assets/ObjectFilter.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ObjectFilter : MonoBehaviour {
+
+    [Serializable]
+    public class Filter
+    {
+        public MoodsManager.moods mood;
+        public int forceToLaneID;
+    }
+    public Filter[] filters;
+
+    public bool CanBeAdded(MoodsManager.moods mood, int landID)
+    {
+        if(filters.Length == 0)
+            return true;
+
+        print(mood);
+
+        foreach (Filter f in filters)
+        {
+            if (f.forceToLaneID == 0 || f.forceToLaneID == landID)
+            {
+                if (f.mood == mood)
+                {
+                    print("este objeti: " + f.mood);
+                    return true;
+                }
+            }
+        }
+        foreach (Filter f in filters)
+        {
+            if (f.mood == MoodsManager.moods.GENERIC)
+                return true;
+        }
+        return false;
+    }
+
+}
