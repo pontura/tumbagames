4821d1babfae96b3663d4c5d7a0fa9ae932d29bd
pontura <pontura@gmail.com>
2017-01-10 13:58:42 -0300

    cutscene_final

--- pungaRaid/src/game/Assets/Texts/Texts.cs
+++ pungaRaid/src/game/Assets/Texts/Texts.cs
@@ -6,13 +6,14 @@ using System.Collections.Generic;
 using System.IO;
 using SimpleJSON;
 
-public class Texts :MonoBehaviour {
+public class Texts : MonoBehaviour
+{
 
     public List<string> tutorial;
     public TextsRuleta ruleta;
     public List<string> combos;
     public List<string> unlocked_levels;
-    
+    public List<string> cutscene_final;
 
     string json_tutorials_Url = "texts/tutorial";
     string json_ruleta_Url = "texts/ruleta";
@@ -24,12 +25,12 @@ public class Texts :MonoBehaviour {
         Encoding utf8 = Encoding.UTF8;
 
         TextAsset file;
-        
+
         file = Resources.Load(json_tutorials_Url) as TextAsset;
         fillArray(tutorial, SimpleJSON.JSON.Parse(file.text)["texts"]);
 
         file = Resources.Load(json_ruleta_Url) as TextAsset;
-        ruleta.Init( SimpleJSON.JSON.Parse(file.text)["texts"]);
+        ruleta.Init(SimpleJSON.JSON.Parse(file.text)["texts"]);
 
         file = Resources.Load(json_moods_Url) as TextAsset;
         GetComponent<MoodsManager>().data.Init(SimpleJSON.JSON.Parse(file.text)["texts"]);
@@ -37,6 +38,7 @@ public class Texts :MonoBehaviour {
         file = Resources.Load(json_genericTexts_Url) as TextAsset;
         fillSimpleTexts(combos, SimpleJSON.JSON.Parse(file.text)["combos"]);
         fillSimpleTexts(unlocked_levels, SimpleJSON.JSON.Parse(file.text)["unlocked_levels"]);
+        fillSimpleTexts(cutscene_final, SimpleJSON.JSON.Parse(file.text)["cutscene_final"]);
     }
     public void LoadDataMinigames(string json_data)
     {
@@ -61,14 +63,14 @@ public class Texts :MonoBehaviour {
     }
     public string GetRandomText(List<string> arr)
     {
-        return arr[ UnityEngine.Random.Range(0,arr.Count) ];
+        return arr[UnityEngine.Random.Range(0, arr.Count)];
     }
     public string ReplaceUserName(string field)
     {
         //if (Data.Instance.userData.username != "")
         //    return field.Replace("[username]", Data.Instance.userData.username);
         //else 
-            return field;
+        return field;
     }
     public string GetFilteredText(List<string> arr, int id)
     {
