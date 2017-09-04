94ce29c6246918786d3da2a9825dd71de6e494dc
pontura <pontura@gmail.com>
2017-09-04 16:14:15 -0300

    cosas

---
+++ femizone/src/Assets/Character.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+	
+	private InputManager inputManager;
+	public Animator anim;
+	public float speed = 10;
+	public states state;
+	public enum states
+	{
+		IDLE,
+		WALK
+	}
+	void Start () {
+		inputManager = GetComponent<InputManager> ();	
+	}
+	void Update () {
+		if ((inputManager.HorizontalDirection != 0 || inputManager.VerticalDirection != 0)) {			
+			if(state != states.WALK)
+				Walk ();
+		}
+		else if (state != states.IDLE)
+			Idle ();
+
+		if (inputManager.HorizontalDirection < 0)
+			transform.localScale = new Vector3 (-1, 1, 1);
+		else if (inputManager.HorizontalDirection > 0)
+			transform.localScale = new Vector3 (1, 1, 1);
+		
+		Vector3 pos = transform.localPosition;
+		pos.x += inputManager.HorizontalDirection * Time.deltaTime * speed;
+		pos.y += inputManager.VerticalDirection * Time.deltaTime * speed;
+		transform.localPosition = pos;
+	}
+	void Walk()
+	{
+		state = states.WALK;
+		anim.Play ("pungaRun");
+	}
+	void Idle()
+	{
+		state = states.IDLE;
+		anim.Play ("pungaTemplate");
+	}
+}
--- femizone/src/Assets/InputManager.cs
+++ femizone/src/Assets/InputManager.cs
@@ -3,12 +3,34 @@ using System.Collections;
 
 public class InputManager : MonoBehaviour {
 		
+	public int HorizontalDirection;
+	public int VerticalDirection;
+
+
 	void Update () {
 
-		if (Input.GetKeyDown (KeyCode.LeftArrow) || Input.GetKeyDown (KeyCode.RightArrow)) {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			HorizontalDirection = -1;
 		}
+		else if (Input.GetKey (KeyCode.RightArrow))
+			HorizontalDirection = 1;
+		else
+			HorizontalDirection = 0;
+		
+		if (Input.GetKey (KeyCode.DownArrow) )
+			VerticalDirection = -1;
+		else if (Input.GetKey (KeyCode.UpArrow))
+			VerticalDirection = 1;
+		else
+			VerticalDirection = 0;
 
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			
+		}
+		if (Input.GetKeyDown (KeyCode.LeftControl)) 
+		{
+			
 		}
 
 	}
