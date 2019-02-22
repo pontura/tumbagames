61f09fdd3a89925979b90379405ead206512d85a
pontura <pontura@gmail.com>
2019-02-22 18:49:09 -0300

    swapper

---
+++ femizone/src/Assets/Swapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swapper : MonoBehaviour
+{
+	public GameObject[] parts;
+
+	void OnEnable()
+	{
+		if (parts.Length == 0)
+			return;
+		
+		GameObject go;
+
+		foreach (GameObject _go in parts) {
+			_go.SetActive (false);
+		}
+
+		go = parts [Random.Range (0, parts.Length)];
+		go.SetActive (true);
+
+	}
+}
