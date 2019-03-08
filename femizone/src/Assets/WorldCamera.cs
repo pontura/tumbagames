9ec6864f007d13a364f22ed9d9f976c8ccf7d2fb
Agustin Pontura <pontura@gmail.com>
2019-03-08 17:29:36 -0300

    anda

--- femizone/src/Assets/WorldCamera.cs
+++ femizone/src/Assets/WorldCamera.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using AlpacaSound.RetroPixelPro;
 
-public class WorldCamera : MonoBehaviour {
+public class WorldCamera : MonoBehaviour
+{
 
-	void Start()
-	{
-//		int total = Screen.height + Screen.width;
-//
-//		int pizelSize = (total * 7) / (1920 + 1080);
-//		print ("(" + total + "*7 /3000) = " + pizelSize);
-//		GetComponent<RetroPixelPro> ().pixelSize = pizelSize;
-	}
-	public void UpdatePosition (float _x) {
-		Vector3 pos = transform.position;
-		Vector3 newPos = pos;
-		newPos.x += _x;
-		transform.position = Vector3.Lerp (pos, newPos, 0.1f);
-	}
+    void Start()
+    {
+        int total = Screen.height + Screen.width;
+
+        int pizelSize = (total * 7) / (1920 + 1080);
+        print("(" + total + "*7 /3000) = " + pizelSize);
+        GetComponent<RetroPixelPro>().pixelSize = pizelSize;
+    }
+    public void UpdatePosition(float _x)
+    {
+        Vector3 pos = transform.position;
+        Vector3 newPos = pos;
+        newPos.x += _x;
+        transform.position = Vector3.Lerp(pos, newPos, 0.1f);
+    }
 }
